85c2bf24304ca14d92621a3a70bc739fd19249e2
Jasmine Chen <jasmine1226@gmail.com>
2019-05-19 20:43:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,9 @@
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  i=0
   languages.each do |style, language_list|
-    puts "#{i}. style = #{style} #{style.type}"
-    puts "#{i}. language = #{language}"
-    i+=1
     language_list.each do |language, info|
-      language[:style] = style.keys.to_S
+      language[:style] = style.to_s
     end
   end
   new_hash
