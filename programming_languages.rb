337e58eb10f7ecf0f124a8aea05864df60026da5
Jasmine Chen <jasmine1226@gmail.com>
2019-05-19 20:37:59 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |name, info|
-      name[:style] = style
-      puts name
+
+      puts info
     end
   end
   new_hash
